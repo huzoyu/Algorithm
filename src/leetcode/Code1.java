9b45ab861f312ce534d375579c94f3014a7e7904
huzoyu <noahahahah@foxmail.com>
2018-11-12 22:07:23 +0800

    习题1

---
+++ src/leetcode/Code1.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. Two Sum
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ *
+ * 解题思路：
+ * 暴力法,两个循环,target减去nums[i],和nums[j]对比,当小于时j++,等于时返回,大于时break
+ *
+ * 最优,遍历存入哈希表,一次完成
+ */
+
+public class Code1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
