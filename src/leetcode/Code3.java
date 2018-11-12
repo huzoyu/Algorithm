4a5734df2ec638b95b634e728a13a585de9a9c53
huzoyu <noahahahah@foxmail.com>
2018-11-12 22:47:27 +0800

    习题3

---
+++ src/leetcode/Code3.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ *
+ * 3. Longest Substring Without Repeating Characters
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Examples:
+ *
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ *
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ *
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring,
+ * "pwke" is a subsequence and not a substring.
+ *
+ *
+ *
+ * 解题思路：
+ * 没有重复字符，遍历加入哈希表，一次遍历
+ */
+public class Code3 {
+}
